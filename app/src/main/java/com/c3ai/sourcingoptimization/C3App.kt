eac0f62688e2c3a10eb1eb9670717932cb13edbe
arhovhannisyan <arusyak.hovhannisyan@dataart.com>
2022-01-13 15:24:48 +0400

    Initial commit

---
+++ app/src/main/java/com/c3ai/sourcingoptimization/C3App.kt
@@ -0,0 +1,7 @@
+package com.c3ai.sourcingoptimization
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class C3App : Application()
---
+++ app/src/main/java/com/c3ai/sourcingoptimization/common/Constants.kt
@@ -0,0 +1,4 @@
+package com.c3ai.sourcingoptimization.common
+
+object Constants {
+}
