5c6d8f8c292def8fc8fe007988471ca667177b30
arhovhannisyan <arusyak.hovhannisyan@dataart.com>
2022-01-19 19:37:42 +0400

    Item Details/Overview tab view

---
+++ app/src/main/java/com/c3ai/sourcingoptimization/presentation/item_details/ItemsDetailPagerAdapter.kt
@@ -0,0 +1,37 @@
+package com.c3ai.sourcingoptimization.presentation.item_details
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import com.c3ai.sourcingoptimization.presentation.item_details.overview.OverviewFragment
+
+const val OVERVIEW_PAGE_INDEX = 0
+const val PO_LINES_PAGE_INDEX = 1
+const val SUPPLIERS_PAGE_INDEX = 2
+
+class ItemsDetailPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+
+    /**
+     * Mapping of the ViewPager page indexes to their respective Fragments
+     */
+    private val tabFragmentsCreators: Map<Int, () -> Fragment> = mapOf(
+        OVERVIEW_PAGE_INDEX to { OverviewFragment() },
+        PO_LINES_PAGE_INDEX to { OverviewFragment() },
+        SUPPLIERS_PAGE_INDEX to {OverviewFragment()}
+    )
+
+    override fun getCount() = tabFragmentsCreators.size
+
+    override fun getItem(position: Int): Fragment {
+        return tabFragmentsCreators[position]?.invoke() ?: throw IndexOutOfBoundsException()
+    }
+
+    override fun getPageTitle(position: Int): CharSequence {
+        return when (position) {
+            OVERVIEW_PAGE_INDEX -> "Overview"
+            PO_LINES_PAGE_INDEX -> "PO Lines"
+            SUPPLIERS_PAGE_INDEX -> "Suppliers"
+            else -> ""
+        }
+    }
+}
---
+++ app/src/main/java/com/c3ai/sourcingoptimization/presentation/item_details/overview/OverviewFragment.kt
@@ -0,0 +1,8 @@
+package com.c3ai.sourcingoptimization.presentation.item_details.overview
+
+import com.c3ai.sourcingoptimization.databinding.FragmentItemDetailsOverviewBinding
+import com.c3ai.sourcingoptimization.presentation.item_details.BaseFragment
+
+class OverviewFragment : BaseFragment<FragmentItemDetailsOverviewBinding>(
+    FragmentItemDetailsOverviewBinding::inflate)  {
+}
