5945ef62529855c5ad92d6e2256a20bcaa5b7835
arhovhannisyan <arusyak.hovhannisyan@dataart.com>
2022-01-21 19:06:54 +0400

    graphs customization

--- app/src/main/java/com/c3ai/sourcingoptimization/presentation/item_details/overview/ItemDetailsOverviewFragment.kt
+++ app/src/main/java/com/c3ai/sourcingoptimization/presentation/item_details/overview/ItemDetailsOverviewFragment.kt
@@ -24,6 +24,10 @@ class ItemDetailsOverviewFragment : BaseFragment<FragmentItemDetailsOverviewBind
         val aaLineChartModel = configureLineChartAndSplineChartStyle()
         val lineChart = binding.lineChartView
         lineChart.aa_drawChartWithChartModel(aaLineChartModel)
+
+        val aaDashedLineChartModel = configureDahsedLineChartAndSplineChartStyle()
+        val dashedLineChart = binding.dashedLineChartView
+        dashedLineChart.aa_drawChartWithChartModel(aaDashedLineChartModel)
     }
 
     private fun setSwitchToButtons() {
@@ -84,99 +88,101 @@ class ItemDetailsOverviewFragment : BaseFragment<FragmentItemDetailsOverviewBind
                 .textOutline("0px 0px contrast"))
     }
 
-    fun configureStepLineChart(): AAChartModel {
-        return AAChartModel()
-            .chartType(AAChartType.Line)
-            .animationType(AAChartAnimationType.Bounce)
-            .backgroundColor("rgba(0,0,0,0)")
-            .legendEnabled(false)
-            .tooltipEnabled(false)
-            .dataLabelsEnabled(false)
-            .markerSymbolStyle(AAChartSymbolStyleType.BorderBlank)
-            .markerRadius(7f)
-            .series(arrayOf(
-                AASeriesElement()
-                    .name("Berlin")
-                    .data(arrayOf(450, 432, 401, 454, 590, 530, 510))
-                    .step("right")
-                , AASeriesElement()
-                    .name("New York")
-                    .data(arrayOf(220, 282, 201, 234, 290, 430, 410))
-                    .step("center")
-                , AASeriesElement()
-                    .name("Tokyo")
-                    .data(arrayOf(120, 132, 101, 134, 90, 230, 210))
-                    .step("left")
-            ))
-    }
-
     private fun configureLineChartAndSplineChartStyle(): AAChartModel {
         val aaChartModel = AAChartModel.Builder(requireContext())
-            .setChartType(AAChartType.Spline)
+            .setChartType(AAChartType.Line)
             .setBackgroundColor("rgba(0,0,0,0)")
             .setDataLabelsEnabled(false)
             .setXAxisGridLineWidth(0f)
             .setYAxisGridLineWidth(0f)
             .setLegendEnabled(false)
-            .setTooltipEnabled(false)
+            .setXAxisVisible(false)
+            .setXAxisLabelsEnabled(false)
+            .setYAxisLineWidth(0f)
+            .setYAxisTitle("")
+//            .setYAxisLabelsEnabled(false)
+            //.setTooltipEnabled(false)
+            //.setTooltipValueSuffix("")
             .setDataLabelsEnabled(false)
             .setTouchEventEnabled(true)
-            .setSeries(
-                AASeriesElement()
-                    .name("Tokyo")
-                    .data(arrayOf(7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6)),
-                AASeriesElement()
-                    .name("NewYork")
-                    .data(arrayOf(0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5)),
-                AASeriesElement()
-                    .name("London")
-                    .data(arrayOf(0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0)),
-                AASeriesElement()
-                    .name("Berlin")
-                    .data(arrayOf(3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8))
-            )
         .build()
 
         aaChartModel
             .markerSymbolStyle(AAChartSymbolStyleType.BorderBlank)
             .markerSymbol(AAChartSymbolType.Circle)
-            .markerRadius(6f)
+            .markerRadius(1f)
 
         val element1 = AASeriesElement()
             .color("#82B0FF")
-            .name("Tokyo")
+            .name("IJK")
             .lineWidth(2f)
             .data(arrayOf(50, 320, 230, 370, 230, 400))
 
         val element2 = AASeriesElement()
             .color("#C799FF")
-            .name("Berlin")
+            .name("ETW")
             .lineWidth(1f)
-            .data(arrayOf(80, 390, 210, 340, 240, 350))
+            .data(arrayOf(20, 290, 310, 140, 240, 350))
 
         val element3 = AASeriesElement()
             .color("#F2950A")
-            .name("New York")
+            .name("ABC")
             .lineWidth(2f)
             .data(arrayOf(100, 370, 180, 280, 260, 300))
 
         val element4 = AASeriesElement()
             .color("#49BFA9")
-            .name("London")
+            .name("FGH")
             .lineWidth(2f)
             .data(arrayOf(130, 350, 160, 310, 250, 268))
 
 
         val element5 = AASeriesElement()
             .color("#A7ADC4")
-            .name("Yerevan")
+            .name("HAE")
             .lineWidth(2f)
-            .data(arrayOf(130, 350, 160, 310, 250, 268))
+            .data(arrayOf(150, 260, 100, 350, 150, 168))
 
 
             aaChartModel
-                .animationType(AAChartAnimationType.Linear)
+                .animationType(AAChartAnimationType.SwingFromTo)
                 .series(arrayOf(element1, element2, element3, element4, element5))
+        return aaChartModel
+    }
+
+    private fun configureDahsedLineChartAndSplineChartStyle(): AAChartModel {
+        val aaChartModel = AAChartModel.Builder(requireContext())
+            .setChartType(AAChartType.Line)
+            .setBackgroundColor("rgba(0,0,0,0)")
+            .setDataLabelsEnabled(false)
+            .setXAxisGridLineWidth(0f)
+            .setYAxisGridLineWidth(0f)
+            .setLegendEnabled(false)
+            .setXAxisVisible(false)
+            .setXAxisLabelsEnabled(false)
+            .setYAxisLineWidth(0f)
+            .setYAxisTitle("")
+//            .setYAxisLabelsEnabled(false)
+            //.setTooltipEnabled(false)
+            //.setTooltipValueSuffix("")
+            .setDataLabelsEnabled(false)
+            .setTouchEventEnabled(true)
+            .build()
+
+        aaChartModel
+            .markerSymbolStyle(AAChartSymbolStyleType.InnerBlank)
+            .markerSymbol(AAChartSymbolType.Circle)
+            .markerRadius(3f)
+
+        val element = AASeriesElement()
+            .color("#008066")
+            .name("")
+            .lineWidth(2f)
+            .data(arrayOf(50, 320, 230, 370, 230, 400))
+
+        aaChartModel
+            .animationType(AAChartAnimationType.SwingFromTo)
+            .series(arrayOf(element))
         return aaChartModel
     }
 }
