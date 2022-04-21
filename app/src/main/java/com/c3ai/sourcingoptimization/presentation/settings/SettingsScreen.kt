2837e7eafb5b50138083eabaa010377f35da56e6
arhovhannisyan <arusyak.hovhannisyan@dataart.com>
2022-04-21 16:22:21 +0400

    Built settings screen

--- app/src/main/java/com/c3ai/sourcingoptimization/presentation/settings/SettingsScreen.kt
+++ app/src/main/java/com/c3ai/sourcingoptimization/presentation/settings/SettingsScreen.kt
@@ -4,15 +4,19 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.style.TextOverflow
@@ -54,20 +58,183 @@ fun SettingsScreen(
     val coroutineScope = rememberCoroutineScope()
     val bottomState = rememberModalBottomSheetState(ModalBottomSheetValue.Hidden)
 
-        Scaffold(
-            scaffoldState = scaffoldState,
-            topBar = {
-                TopAppBar(
-                    onBackButtonClick = onBackButtonClick,
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = {
+            TopAppBar(
+                onBackButtonClick = onBackButtonClick,
+            )
+        },
+        snackbarHost = { C3SnackbarHost(hostState = it) },
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
+            Text(
+                "BA", // TODO!!! get login
+                style = MaterialTheme.typography.h4,
+                color = Color.White,
+                modifier = Modifier.padding(top = 16.dp, bottom = 32.dp)
+            )
+            Divider()
+            Text(
+                text = stringResource(id = R.string.currency),
+                style = MaterialTheme.typography.h4,
+                color = MaterialTheme.colors.primary,
+                modifier = Modifier.padding(top = 16.dp, bottom = 8.dp)
+            )
+
+            val currencyOptions = listOf("USD", "Local")
+            val (selectedCurrencyOption, onCurrencySelected) = remember {
+                mutableStateOf(
+                    currencyOptions[0]
                 )
-            },
-            snackbarHost = { C3SnackbarHost(hostState = it) },
-        ) { innerPadding ->
-            val contentModifier = Modifier.padding(innerPadding)
+            }
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                currencyOptions.forEach { text ->
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 6.dp)
+                            .selectable(
+                                selected = (text == selectedCurrencyOption),
+                                onClick = { onCurrencySelected(text) }
+                            ),
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        RadioButton(
+                            modifier = Modifier.size(20.dp),
+                            selected = (text == selectedCurrencyOption),
+                            onClick = {
+                                onCurrencySelected(text)
+                            },
+                            colors = RadioButtonDefaults.colors(Blue)
+                        )
+                        Text(
+                            text = text,
+                            style = MaterialTheme.typography.body2,
+                            color = MaterialTheme.colors.primary,
+                            modifier = Modifier.padding(start = 16.dp, top = 2.dp)
+                        )
+                    }
+                }
+            }
+            Divider(modifier = Modifier.padding(top = 8.dp))
+
+            Text(
+                text = stringResource(id = R.string.date_format),
+                style = MaterialTheme.typography.h4,
+                color = MaterialTheme.colors.primary,
+                modifier = Modifier.padding(top = 16.dp, bottom = 8.dp)
+            )
+            val dateOptions = listOf("MM/DD/YYYY", "DD/MM/YYYY")
+            val (selectedDateOption, onDateSelected) = remember {
+                mutableStateOf(
+                    dateOptions[0]
+                )
+            }
+
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                dateOptions.forEach { text ->
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 6.dp)
+                            .selectable(
+                                selected = (text == selectedCurrencyOption),
+                                onClick = { onCurrencySelected(text) }
+                            ),
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        RadioButton(
+                            modifier = Modifier.size(20.dp),
+                            selected = (text == selectedDateOption),
+                            onClick = {
+                                onDateSelected(text)
+                            },
+                            colors = RadioButtonDefaults.colors(Blue)
+                        )
+                        Text(
+                            text = text,
+                            style = MaterialTheme.typography.body2,
+                            color = MaterialTheme.colors.primary,
+                            modifier = Modifier.padding(start = 16.dp, top = 2.dp)
+                        )
+                    }
+                }
+            }
+            Divider(modifier = Modifier.padding(top = 8.dp))
+
+            Text(
+                text = stringResource(id = R.string.homepage),
+                style = MaterialTheme.typography.h4,
+                color = MaterialTheme.colors.primary,
+                modifier = Modifier.padding(top = 16.dp, bottom = 8.dp)
+            )
+            val homeOptions = listOf("Search", "Search + Alerts")
+            val (selectedHomeOption, onHomeSelected) = remember {
+                mutableStateOf(
+                    homeOptions[0]
+                )
+            }
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                homeOptions.forEach { text ->
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 6.dp)
+                            .selectable(
+                                selected = (text == selectedCurrencyOption),
+                                onClick = { onCurrencySelected(text) }
+                            ),
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        RadioButton(
+                            modifier = Modifier.size(20.dp),
+                            selected = (text == selectedHomeOption),
+                            onClick = {
+                                onHomeSelected(text)
+                            },
+                            colors = RadioButtonDefaults.colors(Blue)
+                        )
+                        Text(
+                            text = text,
+                            style = MaterialTheme.typography.body2,
+                            color = MaterialTheme.colors.primary,
+                            modifier = Modifier.padding(start = 16.dp, top = 2.dp)
+                        )
+                    }
+                }
+            }
+            Divider(modifier = Modifier.padding(top = 8.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 48.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.logout),
+                    contentDescription = "logout"
+                )
+                Text(
+                    text = stringResource(id = R.string.logout),
+                    style = MaterialTheme.typography.h3,
+                    color = MaterialTheme.colors.primary,
+                    modifier = Modifier.padding(start = 16.dp)
+                )
+            }
+        }
     }
-
 }
-
 
 /**
  * TopAppBar for the Settings screen
